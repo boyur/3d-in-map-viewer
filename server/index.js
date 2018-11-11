43ead15c98d343a412731c97609a79e07c8c6049
boyur <gboyur@gmail.com>
2018-11-11 21:19:48 +0300

    add simple insert in db new files

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const pgp = require("pg-promise")();
 const bodyParser = require('body-parser');
 const upload = require('./upload');
 const app = express();
+const db = pgp("postgres://postgres:postgres@localhost:5432/viewer");
 
 app.use(bodyParser.json());
 
@@ -10,10 +12,25 @@ const fields = [
   { name: 'texture', maxCount: 1 }
 ];
 
+db.one("SELECT NOW()")
+  .then(function (data) {
+    console.log("DATA:", data);
+  })
+  .catch(function (error) {
+    console.log("ERROR:", error);
+  });
+
 app.post('/fileupload', upload.fields(fields), (req, res) => {
-  console.log(req.files);
-  console.log(req.body);
-  res.redirect('/');
+  db.query(`INSERT INTO objects (name, object_url)
+    VALUES ($1, $2);`, [req.body.name, req.files.object[0].filename])
+    .then(function () {
+      console.log("Success:");
+    })
+    .catch(function (error) {
+      console.log("ERROR:", error);
+    });
+
+  res.redirect('http://localhost:3001/');
 });
 
 app.get('/', (req, res) => {
